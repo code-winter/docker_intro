a064d438aae27293fda095391c20b776d6439e1b
Dmitry Popov <ant1h4t3rz@gmail.com>
2022-04-17 20:58:19 +0900

    Second task

---
+++ second/logistic/views.py
@@ -0,0 +1,31 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.viewsets import ModelViewSet
+
+from logistic.models import Product, Stock, StockProduct
+from logistic.serializers import ProductSerializer, StockSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description']
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    filter_backends = [DjangoFilterBackend]
+    # filterset_fields = ['products']
+
+    # def get_queryset(self):
+    #     title = self.request.query_params.get('products')
+    #     try:
+    #         my_query = Product.objects.get(title__icontains=title)
+    #     except ValueError:
+    #         return Product.objects.all()
+    #     product_id = my_query.id
+    #     queryset = Stock.objects.filter(products=product_id)
+    #     return queryset
+
